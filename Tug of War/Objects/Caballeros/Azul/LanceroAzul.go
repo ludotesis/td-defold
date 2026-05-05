b70e60ed3bd05e100c85cfc3177d859fef4b0b5a
Gaston Caminiti <gastoncaminiti@outlook.com>
2026-05-05 14:15:09 -0300

    Crear GameObject Lancero Azul

---
+++ Tug of War/Objects/Caballeros/Azul/LanceroAzul.go
@@ -0,0 +1,41 @@
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 25.0\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"LanceroIdle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 192.0\n"
+  "  y: 192.0\n"
+  "}\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/Atlas/UnidadesAzules.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    z: 1.0
+  }
+}
